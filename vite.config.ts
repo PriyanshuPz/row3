b6f86ce35e1b3c8472d19ce37a217c70fc1e100f
Priyanshu Verma <priyanshuverma@outlook.in>
2025-09-05 21:04:10 +0530

    fix: build error and shinked size by spliting

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          firebase: ["firebase/app", "firebase/auth", "firebase/firestore"],
+          motion: ["framer-motion"],
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+  },
 });
